0cb60b04659e2b47e6c357df959f61274fc9781a
LuffyKing <aderinwale17@gmail.com>
2017-06-27 16:48:01 +0100

    deployment changes

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','+a(i11!e33#v0r90#r67spd*4r)^b7-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nameless-taiga-19449.herokuapp.com','127.0.0.1']
 
 
 # Application definition
